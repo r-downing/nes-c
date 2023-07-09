af99b4e411e442e7cc37133f71d5684838a2230d
Ryan Downing <26515643+r-downing@users.noreply.github.com>
2023-07-09 15:23:09 -0400

    get cmake-based intellisense working

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <emscripten.h>
 
-int main(void) {
+extern int x;
+
+#ifndef TEST_DEFINE
+#warning "TEST_DEFINE not defined"
+#endif
+
+int main(void)
+{
+
+    int j = j; // making sure other warnings work
+    x = j;      // making sure other file built and linked
+
     printf("built %s @ %s\n", __DATE__, __TIME__);
 }
+
--- makefile
+++
@@ -1,6 +0,0 @@
-build: clean
-	mkdir -p build
-	emcc *.c -o build/index.html
-
-clean:
-	rm -rf build/*
