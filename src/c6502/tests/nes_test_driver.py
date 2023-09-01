c5da6246522808131b9e22465362352b7b6f8554
Ryan Downing <26515643+r-downing@users.noreply.github.com>
2023-09-01 17:42:03 -0400

    add driver code to run c6502 tests with nestest.nes and compare logs

---
+++ src/c6502/tests/nes_test_driver.py
@@ -0,0 +1,28 @@
+import sys
+import subprocess
+
+exe, nes_rom, nes_log = sys.argv[1:4]
+
+with open(nes_log, "rt") as fp:
+    nes_log_lines = fp.readlines()
+
+output = subprocess.getoutput(f"{exe} {nes_rom} {len(nes_log_lines)}").split("\n")
+
+
+assert(len(output) == len(nes_log_lines))
+
+import re
+
+pattern = r"(?P<PC>[0-9A-F]{4}).* A:(?P<A>[0-9A-F]{2}) X:(?P<X>[0-9A-F]{2}) Y:(?P<Y>[0-9A-F]{2}) P:(?P<P>[0-9A-F]{2}) SP:(?P<SP>[0-9A-F]{2}).*CYC:(?P<CYC>[0-9]+)"
+
+for i, (line1, line2) in  enumerate(zip(nes_log_lines, output)):
+    m1 = re.match(pattern, line1)
+    m2 = re.match(pattern, line2)
+    print(f"{i + 1:04d}: {line1.strip():100s} {line2.strip()}")
+    gd1 = m1.groupdict()
+    gd2 = m2.groupdict()
+    if gd1 != gd2:
+        print(f"mismatch {[k for k in gd1 if gd1[k] != gd2[k]]}")
+        sys.exit(1)
+
+
---
+++ src/c6502/tests/nes_test_runner.c
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <c6502.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// https://github.com/christopherpow/nes-test-roms/blob/master/other/nestest.log
+
+static uint8_t ram[0x8000] = {0};
+static uint8_t rom[0x4000] = {0};
+
+static uint8_t bus_read(void *, uint16_t addr) {
+    if (addr < sizeof(ram)) {
+        return ram[addr];
+    }
+    return rom[addr & 0x3FFF];  // cartridge rom mirroring
+}
+
+static bool bus_write(void *, uint16_t addr, uint8_t val) {
+    assert(addr < sizeof(ram));
+    ram[addr] = val;
+    return true;
+}
+
+static const C6502BusInterface bus_interface = {.read = bus_read, .write = bus_write};
+
+C6502 c = {.bus = &bus_interface};
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Missing nestest.nes rom path arg");
+        return 1;
+    }
+    if (argc < 3) {
+        printf("Missing step count");
+    }
+
+    const int step_count = atoi(argv[2]);
+
+    FILE *const f = fopen(argv[1], "rb");
+    assert(f);
+    assert(fread(rom, 0x10, 1, f));
+    assert(fread(rom, sizeof(rom), 1, f));
+    fclose(f);
+
+    c6502_reset(&c);
+    c.PC = 0xC000;  // manually set PC for headless
+    c.SR.I = 1;     // ToDo - investigate
+    int line = 0;
+    int tc = -1;  // ToDo - fix offset, add to c6502 struct
+    volatile int bp = 0;
+    for (int i = 0; i < step_count; i++) {
+        tc += c6202_run_next_instruction(&c);
+        printf("%04X    A:%02X X:%02X Y:%02X P:%02X SP:%02X CYC:%d\n", c.PC, c.AC, c.X, c.Y, c.SR.u8, c.SP, tc);
+    }
+    return 0;
+}
